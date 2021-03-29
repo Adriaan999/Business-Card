0049b21e7be8616594b99680fda0d3a07a39a42f
Adriaan999 <Adriaan9@hotmail.co.za>
2021-03-29 09:16:17 +0200

    init

---
+++ AdriaanCard/AdriaanCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdriaanCardApp.swift
+//  AdriaanCard
+//
+//  Created by Adriaan van Schalkwyk on 2021/02/10.
+//
+
+import SwiftUI
+
+@main
+struct AdriaanCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdriaanCard/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  AdriaanCard
+//
+//  Created by Adriaan van Schalkwyk on 2021/02/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0.20, green: 0.29, blue: 0.37)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("Adriaan")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150, height: 150, alignment: .center)
+                    .clipShape(Circle())
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 2)
+                    )
+                Text("Adriaan\nvan Schalkwyk")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white).multilineTextAlignment(.center)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+27 73 133 3323", imageName: "phone.fill")
+                InfoView(text: "adriaan9@hotmail.co.za", imageName: "envelope.fill")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
